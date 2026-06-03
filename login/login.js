9335df44e3664145c853d1cc6a4dc96655356257
sarahfaradla <sarahfaradla@gmail.com>
2026-06-03 21:51:28 +0700

    masuk admin / pelanggan

--- login/login.js
+++ login/login.js
@@ -16,6 +16,10 @@ if (registerForm) {
   registerForm.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    alert("Akun berhasil dibuat!");
+    // Mengambil nilai radio button yang sedang terpilih
+    const selectedRole = document.querySelector('input[name="role"]:checked').value;
+
+    // Menampilkan alert dinamis sesuai pilihan
+    alert(`Akun berhasil dibuat sebagai ${selectedRole.toUpperCase()}!`);
   });
 }
